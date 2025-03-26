6106d402e465aa147da8e9fad6590d1a45db7d0a
origin <viktoryaa2000@mail.ru>
2025-03-26 17:11:38 +0300

    commit new 2

--- src/main/java/org/skypro/counter/model/controller/ShopController.java
+++ src/main/java/org/skypro/counter/model/controller/ShopController.java
@@ -8,13 +8,20 @@ import org.skypro.counter.model.service.StorageService;
 import org.skypro.counter.model.service.SearchService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.List;
 
+@RestController
 public class ShopController {
     private StorageService storageService;
     private SearchService searchServise;
+
+    public ShopController(StorageService storageService, SearchService searchServise){
+        this.storageService = storageService;
+        this.searchServise = searchServise;
+    }
 
     @GetMapping("/products")
     public Collection<Product> getAllProducts() {
